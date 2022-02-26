c06f06ee2e548a8861dfe7491286f75100b080cb
Khoi Tuan <khoituan1412@gmail.com>
2022-02-26 13:58:57 +1000

    setup boilerplate for client & server

---
+++ client/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function App() {
+  const [serverStatus, setServerStatus] = useState("connecting to server...");
+
+  const fetchServerStatus = () => {
+    fetch("http://localhost:8081/watch-app")
+      .then((res) => res.json())
+      .then((res) => setServerStatus(res.message))
+      .catch((error) => setServerStatus(error.message));
+  };
+
+  fetchServerStatus();
+
+  return (
+    <>
+      <p>Server status: {serverStatus}</p>
+    </>
+  );
+}
+
+export default App;

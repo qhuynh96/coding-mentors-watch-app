8d5f255739ce6f73039a0e91b6aaf2a6aaef6345
Henry Nguyen <huyhung1601@gmail.com>
2022-03-15 08:18:18 +1100

    pull

--- server/dist/app.js
+++ server/dist/app.js
@@ -11,12 +11,12 @@ var cors_1 = __importDefault(require("cors"));
 var cookie_parser_1 = __importDefault(require("cookie-parser"));
 var morgan_1 = __importDefault(require("morgan"));
 var http_1 = require("http");
-var app = express_1.default();
-app.use(cors_1.default({
+var app = (0, express_1.default)();
+app.use((0, cors_1.default)({
     origin: "*",
 }));
 // create http server
-var server = http_1.createServer(app);
+var server = (0, http_1.createServer)(app);
 // create the socket io server on top of the http server
 var io = new socket_io_1.Server(server, {
     cors: {
@@ -26,10 +26,10 @@ var io = new socket_io_1.Server(server, {
 io.on("connection", function () {
     console.log("a connection established");
 });
-app.use(morgan_1.default("dev"));
+app.use((0, morgan_1.default)("dev"));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
-app.use(cookie_parser_1.default());
+app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.static(path_1.default.join(__dirname, "public")));
 app.get("/watch-app", (function (req, res) {
     res.status(200).json({
@@ -38,7 +38,7 @@ app.get("/watch-app", (function (req, res) {
 }));
 // catch 404 and forward to error handler
 app.use((function (req, res, next) {
-    next(http_errors_1.default(404));
+    next((0, http_errors_1.default)(404));
 }));
 // error handler
 app.use((function (err, req, res, next) {

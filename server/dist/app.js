3d8784a3aaa40e68f4a60db574100126f22d8a88
Henry Nguyen <huyhung1601@gmail.com>
2022-03-27 10:35:04 +1100

    commit

--- server/dist/app.js
+++ server/dist/app.js
@@ -13,12 +13,12 @@ var morgan_1 = __importDefault(require("morgan"));
 var RoomEvent_1 = require("./RoomEvent");
 var http_1 = require("http");
 var rooms_1 = require("./rooms/rooms");
-var app = express_1.default();
-app.use(cors_1.default({
+var app = (0, express_1.default)();
+app.use((0, cors_1.default)({
     origin: "*",
 }));
 // create http server
-var server = http_1.createServer(app);
+var server = (0, http_1.createServer)(app);
 // create the socket io server on top of the http server
 var io = new socket_io_1.Server(server, {
     cors: {
@@ -27,12 +27,12 @@ var io = new socket_io_1.Server(server, {
 });
 // run once the client connects
 io.on(RoomEvent_1.RoomEvent.connection, function (socket) {
-    var rooms = rooms_1.getRooms();
+    var rooms = (0, rooms_1.getRooms)();
     socket.emit(RoomEvent_1.RoomEvent.SERVER_ROOMS, { rooms: rooms, userId: socket.id });
     socket.on(RoomEvent_1.RoomEvent.CREATE_ROOM, function (_a) {
         var roomId = _a.roomId;
         // create a new room & append it to the current room array
-        var newRoom = rooms_1.createRoom({
+        var newRoom = (0, rooms_1.createRoom)({
             admin: socket.id,
             members: [socket.id],
             roomId: roomId,
@@ -45,7 +45,7 @@ io.on(RoomEvent_1.RoomEvent.connection, function (socket) {
     });
     socket.on(RoomEvent_1.RoomEvent.JOIN_ROOM, function (_a) {
         var roomId = _a.roomId;
-        var res = rooms_1.joinRoom({ roomId: roomId, userId: socket.id });
+        var res = (0, rooms_1.joinRoom)({ roomId: roomId, userId: socket.id });
         socket.join(res.roomId);
         // broadcast when a user connects
         socket.broadcast.emit(RoomEvent_1.RoomEvent.JOINED_ROOM, res);
@@ -53,13 +53,13 @@ io.on(RoomEvent_1.RoomEvent.connection, function (socket) {
     });
     socket.on(RoomEvent_1.RoomEvent.LEAVE_ROOM, function (_a) {
         var roomId = _a.roomId, userId = _a.userId;
-        rooms_1.leaveRoom({ userId: userId, roomId: roomId });
+        (0, rooms_1.leaveRoom)({ userId: userId, roomId: roomId });
     });
 });
-app.use(morgan_1.default("dev"));
+app.use((0, morgan_1.default)("dev"));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
-app.use(cookie_parser_1.default());
+app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.static(path_1.default.join(__dirname, "public")));
 app.get("/watch-app", (function (req, res) {
     res.status(200).json({
@@ -68,7 +68,7 @@ app.get("/watch-app", (function (req, res) {
 }));
 // catch 404 and forward to error handler
 app.use((function (req, res, next) {
-    next(http_errors_1.default(404));
+    next((0, http_errors_1.default)(404));
 }));
 // error handler
 app.use((function (err, req, res, next) {

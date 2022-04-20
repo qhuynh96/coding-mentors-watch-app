6dfddf69bc290ee1baf1ec8034b1baddeae922db
Khoi Tuan <khoituan1412@gmail.com>
2022-04-21 08:31:53 +1000

    dockerize the app (#18)

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:16-alpine
+
+# Set working directory to /cmwa
+WORKDIR /cmwa
+
+# Copy app source
+COPY . .
+
+# Install client dependencies
+WORKDIR /cmwa/client
+RUN npm i
+RUN npm i -g serve
+RUN npm run build
+
+# Install server dependencies
+WORKDIR /cmwa/server
+RUN npm i
+
+WORKDIR /cmwa
+EXPOSE 3000
+EXPOSE 5000
+CMD sh launch.sh
---
+++ ec2_user_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# The bash script for installing Docker & starting the container, to be used in EC2 User Data
+
+yum update -y
+yum install -y docker
+systemctl enable docker.service
+systemctl start docker.service
+docker pull tuannkhoi/cmwa
+docker container run -dit --restart unless-stopped -p 80:3000 -p 5000:5000 tuannkhoi/cmwa:latest
